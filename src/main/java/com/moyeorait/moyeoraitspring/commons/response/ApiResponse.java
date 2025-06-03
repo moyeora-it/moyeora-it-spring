d39e7e17fc13992a799a38cd543d96b28b01c2d2
yuhojae <ho_0214@naver.com>
2025-06-03 13:10:43 +0900

    refactor : data -> items 응답 변수명 변경

--- src/main/java/com/moyeorait/moyeoraitspring/commons/response/ApiResponse.java
+++ src/main/java/com/moyeorait/moyeoraitspring/commons/response/ApiResponse.java
@@ -4,15 +4,15 @@ import com.moyeorait.moyeoraitspring.commons.exception.ExceptionInterface;
 
 public record ApiResponse<T> (
         Status status,
-        T data
+        T items
 ){
 
 
     public static <T> ApiResponse<T> success(){
         return new ApiResponse<>(new Status(true, 200, "success"), null);
     }
-    public static <T> ApiResponse<T> success(T data){
-        return new ApiResponse<>(new Status(true, 200, "success"), data);
+    public static <T> ApiResponse<T> success(T items){
+        return new ApiResponse<>(new Status(true, 200, "success"), items);
     }
 
     public static <T> ApiResponse<T> fail(ExceptionInterface exception){
